ba3d359977323463fe4ad7ccc0951acddfd295be
Andre <andre.jacques.pujol@gmail.com>
2024-05-04 16:29:22 -0500

    Removed auto collective

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,17 +6,7 @@ import poke from "../../img/Pokeball-1.png"
 import auto from "../../img/ac.png";
 
 const projectsArr = [
-  {
-    name: "The Autonomous Collective",
-    img: auto,
-    description:
-      "Made with a group of 4. Full relational PSQL database, server powered by express. React front end. E-commerce site mock up with functioning register, login and  persistent cart between sessions and devices. Also has a fully flushed out Admin page to add products, adjust tags, and edit users. Styled with BootStrap.",
-    githubF:
-      "https://github.com/Autonomous-Collective/Autonomous-Collective-Client",
-    githubB:
-      "https://github.com/Autonomous-Collective/Autonomous-Collective-Backend",
-    deployed: "https://autonomous-collective.netlify.app",
-  },
+ 
   {
     name: "Pokemon Team Builder",
     img: poke,
